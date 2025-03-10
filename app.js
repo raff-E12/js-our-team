d7269bc24c2cdbcaa103e9c1a9294a2eb6e2c651
Raff_12 <raffelecoppola381@gmail.com>
2025-03-10 16:40:20 +0100

    feat: Creazione di Stile Base Per la Pagina

---
+++ app.js
@@ -0,0 +1,4 @@
+// Scopo del Progetto:
+// Dato un array di oggetti rappresentante un team di un’azienda, 
+// creare una pagina dedicata  in cui mostrare una card per ciascun componente.
+// (trovate l’array del team all’interno della cartella in allegato)
